96f3f67c4032745a8c9d3a770d84652ed8119c83
Robert Hernandez <r.hernandez@berkeley.edu>
2014-05-06 13:57:44 -0700

    changed pointer

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Leaflet::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'locations#test'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
