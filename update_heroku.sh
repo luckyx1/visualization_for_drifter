45933a091f7792a24ad473aece5cbfa968445568
Robert Hernandez <r.hernandez@berkeley.edu>
2014-05-06 13:59:04 -0700

    bash update

--- update_heroku.sh
+++ update_heroku.sh
@@ -1,7 +1,7 @@
 createdb mytempdb
 pg_dump -U postgres  leaflet_development | psql -U postgres -d mytempdb
 heroku pg:reset HEROKU_POSTGRESQL_GOLD --app drifter-visualization
-PGUSER=Erick_laptop heroku pg:push mytempdb HEROKU_POSTGRESQL_GOLD_URL --app drifter-visualization
+PGUSER=Erick_laptop heroku pg:push mytempdb\q HEROKU_POSTGRESQL_GOLD_URL --app drifter-visualization
 heroku run rake db:migrate
 dropdb mytempdb
 
