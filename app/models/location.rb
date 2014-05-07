9c0201e95bfcda489a2b0aea032788f0dc9dde6f
Robert Hernandez <r.hernandez@berkeley.edu>
2014-05-07 01:38:49 -0700

    all updated icon

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,4 @@
 class Location < ActiveRecord::Base
-	self.primary_key = "id"
 	attr_accessible :address, :latitude, :longitude, :time, :valid_input, :gps_speed
 	geocoded_by :address
 	after_validation :geocode, :if => :address_changed?
