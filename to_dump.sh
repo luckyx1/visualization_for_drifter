ae56c06b5e0d1aa66c5ac749a3b44178f9005775
Robert Hernandez <r.hernandez@berkeley.edu>
2014-06-29 18:22:34 -0700

    update file

--- to_dump.sh
+++ to_dump.sh
@@ -1,3 +1,3 @@
 #makes a dump file, please rename the .dump file(with like timestatmp)
 rm drifter.dump
-pg_dump -Fc --no-acl --no-owner -h localhost -U Erick_laptop mydbleaf > drifter.dump
+pg_dump -Fc --no-acl --no-owner -h localhost -U Erick_laptop leaflet_development > drifter.dump
