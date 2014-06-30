ed535b2020473f3302dc5fb69f5682fef7b911b5
Robert Hernandez <r.hernandez@berkeley.edu>
2014-06-29 18:13:08 -0700

    data up to june 29, sf and berk

--- heroku_to_local.sh
+++ heroku_to_local.sh
@@ -2,7 +2,7 @@
 bash pull_db_from_heroku.sh
 bash clean_local.sh
 pg_dump mydbleaf  | psql -U Erick_laptop -d leaflet_development
-rake db:seed
+#rake db:seed -- no good seed data
 dropdb mydbleaf
 rm drifter.dump
 pg_dump -Fc --no-acl --no-owner -h localhost -U Erick_laptop leaflet_development > drifter.dump
