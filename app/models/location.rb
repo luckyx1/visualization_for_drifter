369353dd0a03cccfdfeb6acdcdb07f7b4b6dd5e5
Robert Hernandez <r.hernandez@berkeley.edu>
2014-11-01 20:36:33 -0700

    adding csv export

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,14 @@
 class Location < ActiveRecord::Base
 	attr_accessible  :latitude, :longitude, :time, :valid_input, :gps_speed, :drifter_name
+
+	def self.to_csv(options = {})
+  		CSV.generate(options) do |csv|
+    		csv << column_names
+    		all.each do |loc|
+      			csv << loc.attributes.values_at(*column_names)
+    		end
+  		end
+	end
+
 end
 
