578cf4e798f041404420ef9f4671946c46f9d241
Robert Hernandez <roberterickhernandez@gmail.com>
2015-01-25 18:44:37 -0800

    relocated js

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -2,3 +2,123 @@ function dotime(){
 	//<body onLoad="dotime()">	
 	//setTimeout(function(){ dotime();}, 1000);
 }
+
+var Marker = {
+	setup: function(){
+		var mark = $("#map");
+		if (mark.length > 0){//the page has this id on it
+			Marker.setvar();
+		}
+	},
+	setvar: function(){
+		var adjust = L.AwesomeMarkers.icon({
+			    icon: 'fa-adjust',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var asterisk = L.AwesomeMarkers.icon({
+			    icon: 'fa-asterisk',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var down = L.AwesomeMarkers.icon({
+			    icon: 'fa-hand-o-down',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var archive = L.AwesomeMarkers.icon({
+			    icon: 'fa-archive',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var circle = L.AwesomeMarkers.icon({
+			    icon: 'fa-circle',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var cloud = L.AwesomeMarkers.icon({
+			    icon: 'fa-cloud',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var certificate = L.AwesomeMarkers.icon({
+			    icon: 'fa-certificate',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var fighter_jet = L.AwesomeMarkers.icon({
+			    icon: 'fa-fighter-jet',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var coffee = L.AwesomeMarkers.icon({
+			    icon: 'fa-coffee',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var cog = L.AwesomeMarkers.icon({
+			    icon: 'fa-cog',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var bug = L.AwesomeMarkers.icon({
+			    icon: 'fa-bug',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var beer = L.AwesomeMarkers.icon({
+			    icon: 'fa-beer',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var arrow = L.AwesomeMarkers.icon({
+			    icon: 'fa-arrow-circle-o-right',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var dot = L.AwesomeMarkers.icon({
+			    icon: 'fa-dot-circle-o',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var plus = L.AwesomeMarkers.icon({
+			    icon: 'fa-plus-circle',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var rocket = L.AwesomeMarkers.icon({
+			    icon: 'fa-rocket',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var smile = L.AwesomeMarkers.icon({
+			    icon: 'fa-smile-o',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var star = L.AwesomeMarkers.icon({
+			    icon: 'fa-star-o',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var shopping = L.AwesomeMarkers.icon({
+			    icon: 'fa-shopping-cart',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var trophy = L.AwesomeMarkers.icon({
+			    icon: 'fa-trophy',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+			 var tachometer = L.AwesomeMarkers.icon({
+			    icon: 'fa-tachometer',
+			    prefix: 'fa',
+			    markerColor:'orange'
+			  });
+	},
+};
+$(Marker.setup);
+
+
+			
